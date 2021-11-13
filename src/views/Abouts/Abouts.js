f3b5aab7b0503743d3c535ca80aeb839eb04a2dd
Park <62467833+Parksan404@users.noreply.github.com>
2021-11-13 17:42:06 +0700

    'Park'

--- src/views/Abouts/Abouts.js
+++ src/views/Abouts/Abouts.js
@@ -1,9 +1,93 @@
+import { wait } from "@testing-library/dom";
 import React from "react";
-
-export default class Abouts extends React.Component{
-    render(){
-        return(
-            <div>Home</div>
-        );
-    }
+import '../../css/Abouts.css'
+const Abouts = () =>{
+    return(
+        <div>
+            <div class = 'f3'>
+                <div class = 'left'>
+                    <div class = 'font'>Ourteam!</div>
+                        <div class = 'f1'>
+                                    <div class = 'Border'>
+                                        <div class = 'profile'>
+                                            <img src = 'image/Beaver.png'></img>
+                                        </div>
+                                        <div>Narodom Yatnimit</div>
+                                        <div>System Analyst</div>
+                                    </div>
+                                    <div class = 'Border'>
+                                        <div class = 'profile'>
+                                            <img src = 'image/Beaver.png'></img>
+                                        </div>
+                                        <div>Warat Yenjaiprasert</div>
+                                        <div>Developer BackEnd</div>
+                                    </div>
+                            </div>
+                            <div class = 'f2'>
+                                    <div class = 'Border'>
+                                        <div class = 'profile'>
+                                            <img src = 'image/Beaver.png'></img>
+                                        </div>
+                                        <div>Korawit Moonsarn</div>
+                                        <div>System Analyst</div>
+                                    </div>
+                                    <div class = 'Border'>
+                                        <div class = 'profile'>
+                                            <img src = 'image/Beaver.png'></img>
+                                        </div>
+                                        <div>Korawit Moonsarn</div>
+                                        <div>System Analyst</div>
+                                    </div>
+                                    <div class = 'Border'>
+                                        <div class = 'profile'>
+                                            <img src = 'image/Beaver.png'></img>
+                                        </div>
+                                        <div>Korawit Moonsarn</div>
+                                        <div>System Analyst</div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class = 'center'>
+                        <div class = 'header'>
+                            For Education!!
+                        </div>
+                    </div>
+                    <div class = 'txt'>
+                        <div> 
+                            This project make for to learn about blockchain;
+                        </div>
+                        <div> 
+                            Defult project :
+                        </div>
+                    </div>
+                    <div class = 'center'>
+                    <div class='button'>
+                        <img class='img' src = 'image/download.png'></img>
+                        Beeba-Blockchain.docx
+                    </div>
+                    </div>
+                    <div class = 'center'>
+                        <div class = 'header'>
+                            Usage
+                        </div>
+                    </div>
+                    <div class = 'center'>
+                        <div class = 'image'>
+                            <img src = 'image/iconcss.png'></img>
+                            <img src = 'image/icongithub.png'></img>
+                            <img src = 'image/iconinfura2.png'></img>
+                            
+                        </div>
+                        <div class = 'image'>
+                            <img src = 'image/iconmetamask.png'></img>
+                            <img src = 'image/iconreact.png'></img>
+                            <img src = 'image/iconRemix.png'></img>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    );
 }
+export default Abouts
